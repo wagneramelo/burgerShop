84388c91ab0025f59631aa934d8e46bc4138e3f1
Wagner Augusto Melo de Paulo <waso.182@gmail.com>
2021-03-19 23:25:13 -0300

    Added backdrop to Side Drawer

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -5,12 +5,19 @@ import SideDrawer from '../Navigation/SideDrawer/SideDrawer'
 import classes from './Layout.module.css';
 
 class Layout extends Component {
+    state = {
+        showSideDrawer: true
+    }
+
+    sideDrawerClosedHandler = () => {
+        this.setState({showSideDrawer: false});
+    }
 
     render() {
         return(
         <>
             <Toolbar />
-            <SideDrawer />
+            <SideDrawer open={this.state.showSideDrawer} closed={this.sideDrawerClosedHandler}/>
             <main className={classes.Content}> 
                 {this.props.children}
             </main>
--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -1,19 +1,25 @@
 import React from 'react';
 import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
+import Backdrop from '../../UI/Backdrop/Backdrop';
 
 import classes from './SideDrawer.module.css'
 
 const sideDrawer = (props) => {
+    const attachedClasses = [classes.SideDrawer, props.open ? classes.Open : classes.Close];
+    //const attachedClasses = props.open ? [classes.SideDrawer, classes.Open] : [classes.SideDrawer, classes.Close];
     return(
-        <div className={classes.SideDrawer}>
-            <div className={classes.Logo}>
-                <Logo />
+        <>
+            <Backdrop show={props.open} clicked={props.closed}/>
+            <div className={attachedClasses.join(' ')}>
+                <div className={classes.Logo}>
+                    <Logo />
+                </div>
+                <nav>
+                    <NavigationItems />
+                </nav>
             </div>
-            <nav>
-                <NavigationItems />
-            </nav>
-        </div>
+        </>
     );
 
 }
